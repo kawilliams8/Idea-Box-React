45c77183b37055cc2e811d83d15ee671d337cc50
Katie Williams <kawilliams8@gmail.com>
2019-08-09 22:54:25 -0600

    Complete file setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  mode: "development",
+  entry: __dirname + '/app/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build/'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
